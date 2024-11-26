6489a3ef6bd010ad3f0a16d4b271ac42a8fba853
emi <esauvisky@gmail.com>
2024-11-26 10:38:46 -0300

    refactor: update main application logic to use controller functions

--- main.py
+++ main.py
@@ -1,33 +1,31 @@
 #!/usr/bin/env python3
-import sys
-import pyperclip
-from loguru import logger
 import wx
 from api import send_request
+from controller import delete_selection, get_clipboard, handle_selection, paste_clipboard, send_to_clipboard
 from gui import show_selection_dialog
 
 def main():
-    app = wx.App(False)  # Initialize the wx App
-
     # Read data from the clipboard using pyperclip
-    clipboard = pyperclip.paste().strip()
-    if not clipboard:
-        logger.error("Clipboard is empty.")
-        sys.exit(1)
+    handle_selection()
+    clipboard = get_clipboard()
+    try:
+        delete_selection()
 
-    # Call the API
-    improved_sentences = send_request(clipboard)
+        # Call the API
+        improved_sentences = send_request(clipboard)
 
-    # Show selection dialog
-    if improved_sentences:
-        selected_sentence = show_selection_dialog(improved_sentences)
-        improved_sentence = selected_sentence if selected_sentence else clipboard
-    else:
-        improved_sentence = clipboard
+        # Show selection dialog
+        if improved_sentences:
+            selected_sentence = show_selection_dialog(improved_sentences)
+            improved_sentence = selected_sentence if selected_sentence else clipboard
+        else:
+            improved_sentence = clipboard
 
-    # Set data to the clipboard using pyperclip
-    pyperclip.copy(improved_sentence)
-    logger.info(f"Clipboard text '{improved_sentence}' set successfully.")
+        send_to_clipboard(improved_sentence)
+    finally:
+        paste_clipboard()
+
+
 
 if __name__ == "__main__":
     main()
